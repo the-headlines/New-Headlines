ee62555c626b9e5787bfc1e02f8c4fed16f39c8b
williameriksen440 <48436704+williameriksen440@users.noreply.github.com>
2019-03-11 16:39:44 +0400

    Update config.js

--- src/app/configs/config.js
+++ src/app/configs/config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  url:'http://myworks.site/dev/secret_south/api',
+  url:'',
   imgPath:'http://localhost:4200/uploads/images/'
 };
 
-/*module.exports = {
-  url:'http://myworks.site/dev/secret_south/api/',
-  imgPath:'http://myworks.site/dev/secret_south/'
-};*/
