bee69fd0515103a0442af8bdd47563ff5c54444b
williameriksen440 <48436704+williameriksen440@users.noreply.github.com>
2019-05-21 10:58:56 +0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './modules/home/pages/home/home.component';
-import {RoadComponent} from './modules/home/pages/road/road.component';
 import {DealsComponent} from './modules/home/pages/deals/deals.component';
 import {CommerceComponent} from './modules/home/pages/commerce/commerce.component';
 import {FeedbackComponent} from './modules/home/pages/feedback/feedback.component';
@@ -12,13 +11,12 @@ import {SinglePostComponent} from './modules/home/pages/single-post/single-post.
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'road', component: RoadComponent},
     {path: 'commerce', component: CommerceComponent},
     {path: 'deals', component: DealsComponent},
     {path: 'feedback', component: FeedbackComponent},
     {path: 'pictures', component: PicturesComponent},
     {path: 'video', component: VideoComponent},
-    {path: ':id', component: SinglePostComponent},
+    {path: 'post/:id', component: SinglePostComponent},
 ];
 
 @NgModule({
