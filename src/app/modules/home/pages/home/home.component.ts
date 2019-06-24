32dd61b79024b122626c15a03b9f649a71c8da4f
Finn <finn@webmasterfreelanceonlinedev.site>
2019-06-24 18:01:49 +0400

    Dates changes

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -239,4 +239,46 @@ export class HomeComponent implements OnInit {
             return this.messages;
         });
     }
+
+    getDateFormatted(createdDate, onlyDate = true) {
+
+
+        if(!onlyDate) {
+
+            const a = moment(); // date now
+            const b = moment(createdDate); // post created date
+
+            const minutes = a.diff(b, 'minutes');
+            const hours = a.diff(b, 'hours');
+            const days = a.diff(b, 'days');
+            const weeks = a.diff(b, 'weeks');
+            const months = a.diff(b, 'months');
+            const years = a.diff(b, 'years');
+
+            if (hours === 0) {
+                return `${minutes} minutes ago`;
+            } else {
+                if (days === 0) {
+                    return `${hours} ${hours > 1 ? 'hours' : 'hour'} ago`;
+                } else {
+                    if (weeks === 0) {
+                        return `${days} ${days > 1 ? 'days' : 'day'} ago`;
+                    } else if (months === 0) {
+                        return `${weeks} ${weeks > 1 ? 'weeks' : 'week'} ago`;
+                    } else if (years === 0) {
+                        return `${months} ${months > 1 ? 'months' : 'month'} ago`;
+                    }
+                }
+            }
+        }
+
+        else {
+            return moment(createdDate).format('MMMM Do YYYY, h:mm:ss a')
+        }
+
+
+
+
+
+    }
 }
