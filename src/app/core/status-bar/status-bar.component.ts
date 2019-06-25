7b7dccb6b89c93c7aff4e0b4bb729bf706da4dee
Finn <finn@webmasterfreelanceonlinedev.site>
2019-06-25 18:15:45 +0400

    Like buttons changed for  status bar component

--- src/app/core/status-bar/status-bar.component.ts
+++ src/app/core/status-bar/status-bar.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
+import {HomeService} from '../../services/home.service';
 
 @Component({
     selector: 'app-status-bar',
@@ -16,8 +17,13 @@ export class StatusBarComponent implements OnInit {
     currentPost = {};
     openNum: boolean;
     routerUrl: string;
+    upwote = false;
 
-    constructor(private renderer: Renderer2, public router: Router) {
+    constructor(
+        private renderer: Renderer2,
+        public router: Router,
+        private home: HomeService
+    ) {
 
         this.renderer.listen('window', 'click', (e: Event) => {
             if (this.toggleButton && e.target !== this.toggleButton.nativeElement) {
@@ -40,6 +46,16 @@ export class StatusBarComponent implements OnInit {
     show(single) {
         this.isShown = true;
         this.currentPost = single;
+    }
+
+    vote(type, id) {
+        if (type === 'Like') {
+
+            this.upwote = !this.upwote;
+        }
+        this.home.doVoting(id, {voteCategory: type}).subscribe(dt => {
+            // console.log(dt);
+        });
     }
 
     getSingle(id) {
