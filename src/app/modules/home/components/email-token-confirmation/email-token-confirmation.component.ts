aab1e2e657e57bdcdd8692d1ac3456bea8c421aa
Finn <finn@webmasterfreelanceonlinedev.site>
2019-07-26 13:27:15 +0400

    Added token checking for email confirmation page

--- src/app/modules/home/components/email-token-confirmation/email-token-confirmation.component.ts
+++ src/app/modules/home/components/email-token-confirmation/email-token-confirmation.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import * as jwtDecode from 'jwt-decode';
 import {ActivatedRoute, Router} from '@angular/router';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
     selector: 'app-email-token-confirmation',
@@ -16,13 +17,20 @@ export class EmailTokenConfirmationComponent implements OnInit {
         public router: Router,
         private route: ActivatedRoute,
         private jwtHelper: JwtHelperService,
+        private toastr: ToastrService,
+        private cdr: ChangeDetectorRef
     ) {
     }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             const token = params.token || '';
-            if (token) {
+
+            const parts = token.split('.');
+            if (parts.length !== 3) {
+                this.toastr.error('The token is invalid');
+                this.cdr.detectChanges();
+            } else if (token) {
                 this.tokenExpired = this.jwtHelper.isTokenExpired(token);
                 this.userData = jwtDecode(token);
 
