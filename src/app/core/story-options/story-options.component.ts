d94a8469dd1e027b2f054d38b08e17a440555522
Finn <finn@webmasterfreelanceonlinedev.site>
2019-08-12 16:56:59 +0400

    Attempt to fix copy link

--- src/app/core/story-options/story-options.component.ts
+++ src/app/core/story-options/story-options.component.ts
@@ -71,32 +71,31 @@ export class StoryOptionsComponent implements OnInit {
     copyLink(link, input) {
         console.log(navigator.userAgent);
         // if (this.isOS()) {
-        // let oldContentEditable = input.contentEditable,
-        //     oldReadOnly = input.readOnly,
-        //     range = document.createRange();
-        //
-        // input.contentEditable = true;
-        // input.readOnly = false;
-        // range.selectNodeContents(input);
-        //
-        // let s = window.getSelection();
-        // s.removeAllRanges();
-        // s.addRange(range);
-        //
-        // input.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
-        //
-        // input.contentEditable = oldContentEditable;
-        // input.readOnly = oldReadOnly;
-        input.setSelectionRange(0, input.value.length);
-        console.log('ios');
+        let oldContentEditable = input.contentEditable,
+            oldReadOnly = input.readOnly,
+            range = document.createRange();
+
+        input.contentEditable = true;
+        input.readOnly = false;
+        range.selectNodeContents(input);
+
+        let s = window.getSelection();
+        s.removeAllRanges();
+        s.addRange(range);
+
+        input.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+        input.contentEditable = oldContentEditable;
+        input.readOnly = oldReadOnly;
+
 
         document.execCommand('copy');
-
+        console.log('ios');
 
         // } else {
-
-        // input.select();
-        // document.execCommand('copy');
+        //
+        //     input.select();
+        //     document.execCommand('copy');
         // }
     }
 
