11c0175ecd7497bad7b9244043ea4d9e962aada7
Finn Galanos <finn@webmasterfreelanceonlinedev.site>
2019-08-30 13:58:50 +0400

    Forgot password form changes

--- src/app/modules/home/components/forgot-password/forgot-password.component.ts
+++ src/app/modules/home/components/forgot-password/forgot-password.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../../../services/auth.service';
 import {ActivatedRoute} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
     selector: 'app-forgot-password',
@@ -12,21 +13,38 @@ export class ForgotPasswordComponent implements OnInit {
     forgotPassForm: FormGroup;
     confirmNewPassForm: FormGroup;
     emailToken;
+    formSubmitted = false;
 
     constructor(
         private fb: FormBuilder,
         public auth: AuthService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private toastr: ToastrService
     ) {
         this.forgotPassForm = this.fb.group({
-            email: ['']
+            email: ['', Validators.required]
         });
 
         this.confirmNewPassForm = this.fb.group({
-            email: [''],
-            newPass: [''],
-            newPassConfirm: ['']
+            token: [''],
+            newPassword: ['', Validators.compose([Validators.required])],
+            newPassConfirm: ['', Validators.required],
+        }, {
+            validator: this.comparePasswords
         });
+    }
+
+    comparePasswords(control: AbstractControl) {
+        const password: string = control.get('newPassword').value; // get password from our password form control
+        const confirmPassword: string = control.get('newPassConfirm').value; // get password from our confirmPassword form control
+
+        // compare is the password math
+        if (password && confirmPassword && password !== confirmPassword) {
+            // if they don't match, set an error in our confirmPassword form control
+            control.get('newPassConfirm').setErrors({passwordsMismatch: true});
+        } else {
+            control.get('newPassConfirm').setErrors({});
+        }
     }
 
     ngOnInit() {
@@ -35,23 +53,44 @@ export class ForgotPasswordComponent implements OnInit {
         if (params.hasOwnProperty('token')) {
 
             this.emailToken = params.token;
+            this.confirmNewPassForm.patchValue({token: params.token});
         }
-        console.log(this.emailToken);
     }
 
+
     sendEmail() {
+        this.formSubmitted = true;
         if (this.forgotPassForm.valid) {
 
             this.auth.sendForgotPassRequest(this.forgotPassForm.value).subscribe(dt => {
                 this.forgotPassForm.reset();
+                this.formSubmitted = false;
+                this.toastr.success(dt['message']); // 'We will shortly send you an email describing your next steps. Thank you.'
             });
         }
     }
 
     sendNewPass() {
-        this.auth.sendNewPass(this.confirmNewPassForm.value).subscribe(dt => {
-            this.confirmNewPassForm.reset();
-        });
+        if (this.confirmNewPassForm.valid) {
+
+            this.auth.sendNewPass(this.confirmNewPassForm.value).subscribe(dt => {
+                this.confirmNewPassForm.reset();
+            });
+        }
+
+    }
+
+
+    get emailControl() {
+        return this.forgotPassForm.get('email');
+    }
+
+    get passControl() {
+        return this.confirmNewPassForm.get('newPassword');
+    }
+
+    get passConfirmControl() {
+        return this.confirmNewPassForm.get('newPassConfirm');
     }
 
 }
